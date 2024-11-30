49e4255f42f67e8b49ac22ba64564d5862fa0702
lazyymohitt <async.mohitt@gmail.com>
2024-11-30 22:25:07 +0530

    Added My Scroll Animation Website Using HTML,CSS & JS

---
+++ script.js
@@ -0,0 +1,67 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+const images = [];
+let imagesLoaded = 0;
+
+const frames = {
+  currentIndex: 0,
+  maxIndex: 1044,
+};
+
+function preloader() {
+  for (var i = 1; i <= frames.maxIndex; i++) {
+    const imageUrl = `./loadingImages2/frame_${i
+      .toString()
+      .padStart(4, "0")}.jpeg`;
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => {
+      imagesLoaded++;
+      if (imagesLoaded === frames.maxIndex) {
+        loadImages(frames.currentIndex);
+        startAnimation()
+      }
+    }
+    images.push(img);
+  }
+}
+function loadImages(index) {
+  if (index >= 0 && index <= frames.maxIndex) {
+    let img = images[index];
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const scaleX = canvas.width / img.width;
+    const scaleY = canvas.height / img.height;
+    const scale = Math.max(scaleX, scaleY);
+
+    const newWidth = img.width * scale;
+    const newHeight = img.height * scale;
+
+    const offsetX = (canvas.width - newWidth) / 2;
+    const offsetY = (canvas.height - newHeight) / 2;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.imageSmoothingEnabled = true;
+    context.imageSmoothingQuality = "high";
+    context.drawImage(img, offsetX, offsetY, newWidth, newHeight);
+    frames.currentIndex = index;
+  }
+}
+ function startAnimation(){
+  var tl = gsap.timeline({
+    scrollTrigger:{
+      trigger:".parent",
+      start:"top top",
+      scrub:2
+    }
+  })
+  tl.to(frames,{
+    currentIndex :frames.maxIndex,
+    onUpdate:  function(){
+      loadImages(Math.floor(frames.currentIndex))
+    }
+  })
+ }
+preloader();
